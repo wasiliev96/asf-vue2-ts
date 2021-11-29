b35e71e6bfa9086997f09313a033187ec2fbc660
wasiliev96 <wasiliev.cloud@gmail.com>
2021-11-29 21:46:41 +0200

    feat: base api calls

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,27 @@
 
 <script lang="ts">
 import Vue from 'vue';
+import {getSearchId, getTickets} from "@/services/AviaSalesService";
+import {Ticket} from "./services/types";
+type DataType = {
+  tickets: null | Ticket[]
+}
 
 export default Vue.extend({
   name: 'App',
-  components: {}
+  components: {},
+  data: (): DataType => ({
+    tickets: null,
+  }),
+  mounted() {
+    getSearchId()
+        .then(res => res.data)
+        .then(data => data.searchId)
+        .then(getTickets)
+        .then(res => res.data)
+        .then(data => this.tickets = data.tickets)
+        .catch(error => console.error(error))
+  }
 });
 </script>
 
--- src/services/AviaSalesService.ts
+++ src/services/AviaSalesService.ts
@@ -1,12 +1,25 @@
-import axios from 'axios';
+import axios, {AxiosPromise} from 'axios';
+import {SearchIDResponse, Ticket} from "@/services/types";
 
-const BASE_URL = 'https://front-test.beta.aviasales.ru/';
+const BASE_URL = 'https://front-test.beta.aviasales.ru';
 
 const aviaSalesAPIClient = axios.create({
     baseURL: BASE_URL,
     timeout: 2000,
 });
 
+const getSearchId = (): AxiosPromise<SearchIDResponse> => {
+    return aviaSalesAPIClient.get('/search');
+}
+
+const getTickets = (searchId: string): AxiosPromise<{ tickets: Ticket[], stop: boolean }> => {
+    const params = {searchId}
+    return aviaSalesAPIClient.get('/tickets', {params})
+}
+
+
 export {
     aviaSalesAPIClient,
+    getSearchId,
+    getTickets
 }
