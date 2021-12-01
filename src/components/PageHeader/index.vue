0e94ee2048a0e280c9f79cb1fe332f669164d8c3
wasiliev96 <wasiliev.cloud@gmail.com>
2021-12-01 09:15:37 +0200

    feat: add page header

---
+++ src/components/PageHeader/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <header class="page-header">
+    <svg-icon icon-name="logo" class-name="logo"></svg-icon>
+  </header>
+</template>
+<script lang="ts">
+import {Component, Vue} from "vue-property-decorator";
+import SvgIcon from "@/components/SvgIcon/index.vue";
+
+@Component({
+  name: "PageHeader",
+  components: {
+    SvgIcon
+  }
+})
+export default class PageHeader extends Vue {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.page-header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-block: 50px;
+}
+
+.logo {
+  width: 60px;
+  height: 60px;
+}
+</style>
